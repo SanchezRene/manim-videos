8cdc95edcfc1535dd779d99c701a7e984f97c7b7
SanchezRene <rene.sanchez6502@gmail.com>
2024-09-22 22:09:26 -0300

    collection_types.py

---
+++ collection_types.py
@@ -0,0 +1,24 @@
+from manim import *
+
+class CollectionTypes(Scene):
+    def construct(self):
+        # Title
+        title = Text("Collection Types", color=WHITE).shift(6 * UP)
+        self.play(Write(title))
+
+        # Single Circle
+        circle = Circle(fill_opacity=0.2,fill_color=YELLOW,color=WHITE).scale(0.4)
+
+        ## LinearGroup
+        linear_group = VGroup(*[circle.copy() for _ in range(5)])
+        # Arrage the circles in a row
+        linear_group.arrange(RIGHT, buff=0.5)
+        # Label
+        linear_label = Text("Linear", color=WHITE).next_to(circle, UP*2)
+        # Lines between circles
+        linear_lines = VGroup(*[Line(linear_group[i].get_right(), linear_group[i+1].get_left(), color=WHITE) for i in range(4)])
+
+        self.add(linear_group)
+        self.add(linear_lines)
+        self.play(Write(linear_label))
+        self.wait(3)
